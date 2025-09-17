1faa3773fca3d575461375aa6663428fbdabc7da
Matthew Maxwell <maxwellmattryan@gmail.com>
2025-09-17 00:13:04 -0500

    Format code

--- src/parameter.cpp
+++ src/parameter.cpp
@@ -1,5 +1,5 @@
-#include "logger.h"
 #include "parameter.h"
+#include "logger.h"
 
 juce::AudioProcessorValueTreeState::ParameterLayout cortex::ParameterManager::CreateParameterLayout(std::map<juce::String, cortex::Parameter> parameters)
 {
